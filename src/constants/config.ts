39923bb1b99703fc383efde823d6f0e08010bf38
Praful Kumar <prafulk9155@gmail.com>
2024-05-19 14:12:41 +0530

    Added portfolio

---
+++ src/constants/config.ts
@@ -0,0 +1,92 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Praful Kumar — Portfolio",
+    fullName: "Praful Kumar",
+    email: "Prafulk9155@gmail.com",
+  },
+  hero: {
+    name: "Praful Kumar",
+    p: ["Versatile professional",
+    " with expertise in full-stack Development."],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `I'm a skilled software developer with experience in TypeScript and
+      JavaScript, and expertise in frameworks like React, Node.js, and library like Angular. Skilled in Angular and React for frontend, Php and Node js for backend, and proficient in MySQL for database management. Extensive experience in DevOps, server administration, and comprehensive DBMS management.!`,
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    feedbacks: {
+      p: "What others say",
+      h2: "Testimonials.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,282 @@
+import type {
+  TNavLink,
+  TService,
+  TTechnology,
+  TExperience,
+  TTestimonial,
+  TProject,
+} from "../types";
+
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  oges,
+  newton_school,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  docker,
+  meta,
+  // starbucks,
+  // tesla,
+  shopify,
+  internshala,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  devEng,
+  devops,
+} from "../assets";
+
+export const navLinks: TNavLink[] = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services: TService[] = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Devops Engineer",
+    icon: devEng,
+  },
+];
+
+const technologies: TTechnology[] = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences: TExperience[] = [
+  {
+    title: "MERN Stack Traniee",
+    companyName: "Newton School",
+    icon: newton_school,
+    iconBg: "#E6DEDD",
+    date: "Sept 2021 - Mar 2022",
+    points: [
+      "Demonstrated expertise in MongoDB, Express.js, React.js, and Node.js honed through hands-on experience at Newton School.",
+
+      "Successfully contributed to multiple projects during the tenure, showcasing the ability to work effectively within a team environment",
+
+      "Engineered scalable applications capable of handling diverse user requirements, ensuring optimal performance and user satisfaction",
+    ],
+  },
+  {
+    title: "Web Developer Intern",
+    companyName: "Command Consultancy Software Pvt Ltd",
+    icon: "",
+    iconBg: "#383E56",
+    date: "Mar 2022 - May 2023",
+    points: [
+      "Completed an 8 week internship training.",
+      
+      "Worked on different technologies related to web development.",
+     
+      "Completed projects and get certified by the company."
+    ],
+  },
+  {
+    title: "Web Developer Traniee",
+    companyName: "Internshala",
+    icon: internshala,
+    iconBg: "#383E56",
+    date: "Mar 2022 - May 2023",
+    points: [
+      "Developing and maintaining web applications using HTML, CSS, Javascript and React Js",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Project Engineer",
+    companyName: "Oges Solution Pvt. Ltd",
+    icon: oges,
+    iconBg: "#E6DEDD",
+    date: "Nov 2022 - May 2023",
+    points: [
+      
+"      Experienced web developer specializing in PHP for backend development, with a proficiency in SQL databases."      ,
+"      Skilled in creating dynamic and responsive user interfaces using Angular, HTML and CSS for frontend development." ,    
+      
+"      Passionate about crafting efficient and innovative server side solution as task automation with cronjob using shell scripts."    ],
+  },
+  {
+    title: "Project Engineer 3",
+    companyName: "Oges Solution Pvt. Ltd.",
+    icon: oges,
+    iconBg: "#383E56",
+    date: "June 2023 - Present",
+    points: [
+     " Versatile professional with expertise in full-stack development, encompassing frontend and backend technologies."
+      ,
+     " Skilled in Angular and React for frontend, Node JS for backend, and proficient in MySQL for database management."
+      ,
+      "Extensive experience in DevOps, server administration, and comprehensive DBMS management."
+    ],
+  },
+];
+
+const testimonials: TTestimonial[] = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects: TProject[] = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    sourceCodeLink: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    sourceCodeLink: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
